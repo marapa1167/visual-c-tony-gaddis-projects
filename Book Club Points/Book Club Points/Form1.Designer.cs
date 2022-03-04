1f9b0d619d4fc5f248b11ea57ee8cf6424f91d36
marapa1167 <100727085+marapa1167@users.noreply.github.com>
2022-03-04 21:56:12 +0100

    Add files via upload

---
+++ Book Club Points/Book Club Points/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Book_Club_Points
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputPromptLbl = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputPromptLbl
+            // 
+            this.inputPromptLbl.AutoSize = true;
+            this.inputPromptLbl.Location = new System.Drawing.Point(86, 25);
+            this.inputPromptLbl.Name = "inputPromptLbl";
+            this.inputPromptLbl.Size = new System.Drawing.Size(317, 17);
+            this.inputPromptLbl.TabIndex = 0;
+            this.inputPromptLbl.Text = "Enter  the number of books you have purchased:";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(432, 25);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(100, 22);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(70, 87);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(94, 78);
+            this.checkButton.TabIndex = 2;
+            this.checkButton.Text = "Check your bonus points";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(230, 87);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(89, 78);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(372, 87);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(89, 78);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 191);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.inputPromptLbl);
+            this.Name = "Form1";
+            this.Text = "Book Club Points";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label inputPromptLbl;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Book Club Points/Book Club Points/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book_Club_Points
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            int input;
+
+            if (int.TryParse(inputTextBox.Text, out input))
+            {
+                if (input == 0)
+                {
+                    MessageBox.Show("Sorry, no bonus points!");
+                }
+                else if (input == 1)
+                {
+                    MessageBox.Show("You earned 5 bonus points!");
+                }
+                else if (input == 2)
+                {
+                    MessageBox.Show("You earned 15 bonus points!");
+                }
+                else if (input == 3)
+                {
+                    MessageBox.Show("You earned 30 bonus points!");
+                }
+                else if (input >= 4)
+                {
+                    MessageBox.Show("You earned 60 bonus points!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter the number of books you have purchased!");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Text = String.Empty;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            string text = "Are you sure you want to exit?";
+            string title = "Exit";
+
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(text, title, buttons, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else
+            {
+                inputTextBox.Text = String.Empty;
+            }
+            
+        }
+    }
+}
