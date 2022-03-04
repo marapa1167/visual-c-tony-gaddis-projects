f6bf49550367f84ead2718fc86e0b24fb11ff12a
marapa1167 <100727085+marapa1167@users.noreply.github.com>
2022-03-04 22:57:44 +0100

    Add files via upload

---
+++ Language Translator/Language Translator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Language_Translator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonItalian_Click(object sender, EventArgs e)
+        {
+            labelOutput.Text = "Buongiorno";
+        }
+
+        private void buttonSpanish_Click(object sender, EventArgs e)
+        {
+            labelOutput.Text = "Buenos dias";
+        }
+
+        private void buttonGerman_Click(object sender, EventArgs e)
+        {
+            labelOutput.Text = "Guten Morgen";
+        }
+    }
+}
